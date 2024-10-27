2511ebad3039f0a41726d410e6e258e4c759a37c
jordanthrelfall <jordanjoannthrelfall@gmail.com>
2024-10-27 15:51:48 -0400

    added animations, fixed some bugs , all that fun stuff

---
+++ Assets/EnergyFlow.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyFlow : MonoBehaviour
+{
+    public GameObject pvToBattery;
+    public GameObject batteryToInverter;
+    public GameObject gridToHouse;
+    public GameObject lamp;
+
+    public MessageHandler message;
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (message.powerSource)
+        {
+            case "PV / Battery":
+                pvToBattery.SetActive(true);
+                batteryToInverter.SetActive(false);
+                gridToHouse.SetActive(false);
+                break;
+            case "PV / Battery / Inverter":
+                pvToBattery.SetActive(true);
+                batteryToInverter.SetActive(true);
+                gridToHouse.SetActive(false);
+                break;
+            case "PV / Battery / Grid":
+                pvToBattery.SetActive(true);
+                gridToHouse.SetActive(true);
+                batteryToInverter.SetActive(false);
+                break;
+            case "Battery / Inverter":
+                batteryToInverter.SetActive(true);
+                pvToBattery.SetActive(false);
+                gridToHouse.SetActive(false);
+                break;
+            case "Grid":
+                gridToHouse.SetActive(true);
+                pvToBattery.SetActive(false);
+                batteryToInverter.SetActive(false);
+                break;
+            default:
+                break;
+        }
+        
+        if (message.load == "DC")
+        {
+            lamp.SetActive(false);
+        }
+        else
+        {
+            lamp.SetActive(true);
+        }
+    }
+}
--- Assets/ShieldControls.cs
+++ Assets/ShieldControls.cs
@@ -76,31 +76,92 @@ public class ShieldControls : MonoBehaviour
         {
             GameObject child = pwrToggleGroup.transform.Find("DayDC").gameObject;
             child.SetActive(true);
+            child = pwrToggleGroup.transform.Find("DayAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("DayDCAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightDC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightDCAC").gameObject;
+            child.SetActive(false);
         }
         else if ((tod == "Day") && (ld == "AC"))
         {
             GameObject child = pwrToggleGroup.transform.Find("DayAC").gameObject;
             child.SetActive(true);
+            child = pwrToggleGroup.transform.Find("DayDC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("DayDCAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightDC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightDCAC").gameObject;
+            child.SetActive(false);
         }
         else if ((tod == "Day") && (ld == "AC/DC"))
         {
             GameObject child = pwrToggleGroup.transform.Find("DayDCAC").gameObject;
             child.SetActive(true);
+            child = pwrToggleGroup.transform.Find("DayDC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("DayAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightDC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightDCAC").gameObject;
+            child.SetActive(false);
         }
         if ((tod == "Night") && (ld == "DC"))
         {
             GameObject child = pwrToggleGroup.transform.Find("NightDC").gameObject;
             child.SetActive(true);
+            child = pwrToggleGroup.transform.Find("DayDC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("DayAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("DayDCAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightDCAC").gameObject;
+            child.SetActive(false);
         }
         else if ((tod == "Night") && (ld == "AC"))
         {
             GameObject child = pwrToggleGroup.transform.Find("NightAC").gameObject;
             child.SetActive(true);
+            child = pwrToggleGroup.transform.Find("DayDC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("DayAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("DayDCAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightDC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightDCAC").gameObject;
+            child.SetActive(false);
         }
         else if ((tod == "Night") && (ld == "AC/DC"))
         {
             GameObject child = pwrToggleGroup.transform.Find("NightDCAC").gameObject;
             child.SetActive(true);
+            child = pwrToggleGroup.transform.Find("DayDC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("DayAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("DayDCAC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightDC").gameObject;
+            child.SetActive(false);
+            child = pwrToggleGroup.transform.Find("NightAC").gameObject;
+            child.SetActive(false);
+
         }
     }
 }
